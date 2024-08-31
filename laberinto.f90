a368537f94543a410f565c8544a7dfb758777dd5
Rey_Sombra <lblanco226@alumnos.iua.edu.ar>
2024-08-31 01:42:50 -0300

    laberinto

--- laberinto.f90
+++ laberinto.f90
@@ -1,16 +1,32 @@
 program laberinto
     implicit none
     integer, parameter :: n=6
-    integer i, a
+    integer i, a, current_pos
     integer :: random_value
-    character(len=3) :: sal1(n), sal2(n), nom1(n), nom2(n), nom3(n), com(n), desc(n), hab(n), sal3(n), res, aux, j, current_pos
+    integer :: sal1(n), habi(n)
+    character(len=1) :: nom1(n), nom2(n), nom3(n)
+    character(len=3) :: sal2(n), com(n), desc(n), sal3(n), hab(n), res, aux, j
+    character(len=100) :: comand, descno, descsu, desces, descoe, descar, descab, irno, irsu, ires,  iroe, irar, irab, dummy
     hab= (/"1","2","3","4","5","6"/)
     sal2= (/"e", "1", "2", "3", "4", "5"/)
     sal3= (/"2", "3", "4", "5", "6", "s"/)
     com= (/"no", "su", "es", "oe", "ar", "ab"/)
-    sal1=(/"n","n","n","n","n","n"/)
+    sal1=(/99,99,99,99,99,99/)
+    habi= (/ 1, 2, 3, 4, 5, 6/)
 
-    current_pos="1"
+    current_pos= 1
+    descno = "Hay una salida hacia el norte"
+    descsu = "Hay una salida hacia el sur"
+    desces = "Hay una salida hacia el este"
+    descoe = "Hay una salida hacia el oeste"
+    descar = "Hay un agujero en el techo"
+    descab = "Hay un agujero en el suelo"
+    irno = "Te diriges por un pasadizo hacia el norte hasta llegar a una nueva sala"
+    irsu = "Te diriges por un pasadizo hacia el sur hasta llegar a una nueva sala"
+    ires = "Te diriges por un pasadizo hacia el este hasta llegar a una nueva sala"
+    iroe = "Te diriges por un pasadizo hacia el oeste hasta llegar a una nueva sala"
+    irar = "Trepas hasta la siguiente sala"
+    irab = "Deciendes hasta la siguiente sala"
 
 
 
@@ -21,64 +37,374 @@ program laberinto
     end do
 nom2(1)="e"
     do i = 2, n
+        do
         random_value = generate_random_integer()
+        if (nom1(i) /= hab(random_value)) then
         nom2(i) = hab(random_value)
+        exit
+        end if
+        end do
     end do
 nom3(6)="s"
     do i = 1, n-1
+        do
         random_value = generate_random_integer()
-        nom3(i) = hab(random_value)
+        if (nom1(i) /= hab(random_value)) then
+            if (nom2(i) /= hab(random_value)) then
+            nom3(i) = hab(random_value)
+            exit
+            end if
+        end if
+        end do
     end do
     do i = 1, n
-        select case (i)
-        case(1)
-            j="1"
-        case(2)
-            j="2"
-        case(3)
-            j="3"
-        case(4)
-            j="4"
-        case(5)
-            j="5"
-        case(6)
-            j="6"
-            end select
-        if (sal1(i)=="n") then
+
+        if (sal1(i)==99) then
             do
             random_value = generate_random_integer()
-            sal1(i) = hab(random_value)
-            aux=sal1(i)
-            select case (aux)
-            case("1")
-                a=1
-            case("2")
-                a=2
-            case("3")
-                a=3
-            case("4")
-                a=4
-            case("5")
-                a=5
-            case("6")
-                a=6
-                end select
-            if (sal1(a)=="n") then
-            sal1(a)=j
+            sal1(i) = habi(random_value)
+            a=sal1(i)
+            if (sal1(a)==99) then
+            sal1(a)=i
             exit
             end if
             end do
         end if
     end do
 
-    print*, "sal 1 es:", sal1, "asi"
-    print*, nom1
-    print*, nom2
-    print*, nom3
-    print*, hab
-    print*, sal3
-    print*, com
-    print*, sal2
+
+comand="mirar"
+print*
+print*, "LABERINTO DE TEXTO V1.00"
+print*, "Rey Sombra"
+print*
+print*, "ADVERTENCIA: Cada vez que ingreses al laberinto este habra cambiado completamente"
+print*, "Hay 131,621,703,842,267,136 combinaciones distintas del laberinto, y todas tienen solucion"
+print*, "Se recomienda fuertemente el uso de papel y lapiz"
+print*
+print*
+print*, "Te encuentras en un laberinto subterraneo lleno de salas identicas"
+print*,  "No sabes como llegaste hasta aqui, solo te queda buscar una salida"
+print*, "Podes usar los comandos ARRIBA, ABAJO, NORTE, SUR, ESTE, OESTE, MIRAR, SALIR y CERRAR. Asegurate de tipear bien el comando"
+print*
+
+
+DO 
+
+    select case (nom2(current_pos))
+    case ("e")
+        print*, "En esta habitacion se encuentra la entrada que esta bloqueada"
+    case ("1")
+        print*, descno
+    case ("2")
+        print*, descsu
+    case ("3")
+        print*, desces
+    case ("4")
+        print*, descoe
+    case ("5")
+        print*, descar
+    case ("6")
+        print*, descab
+    end select
+
+
+        
+        select case (nom1(current_pos))
+        case ("1")
+            print*, descno
+        case ("2")
+            print*, descsu
+        case ("3")
+            print*, desces
+        case ("4")
+            print*, descoe
+        case ("5")
+            print*, descar
+        case ("6")
+            print*, descab
+        end select
+
+
+        select case (nom3(current_pos))
+                    
+        case ("s")
+            print*, "En esta habitacion se encuentra la salida!, puedes SALIR del laberinto"
+        case ("1")
+            print*, descno
+        case ("2")
+            print*, descsu
+        case ("3")
+            print*, desces
+        case ("4")
+            print*, descoe
+        case ("5")
+            print*, descar
+        case ("6")
+            print*, descab
+        end select
+
+        print*
+    read*, comand
+
+SELECT CASE (comand)
+
+case ("norte")
+    print*
+If (nom1(current_pos)=="1") then
+    print*, irno
+    current_pos=sal1(current_pos)
+else if (nom2(current_pos)=="1") then
+        print*, irno
+        SELECT case (sal2(current_pos))
+        case ("1")
+        current_pos=1
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+    end select
+else if (nom3(current_pos)=="1") then
+    print*, irno
+    SELECT case (sal3(current_pos))
+    case ("2")
+    current_pos=2
+    case ("3")
+    current_pos=3
+    case ("4")
+    current_pos=4
+    case ("5")
+    current_pos=5
+    case ("6")
+    current_pos=6
+    end select
+else 
+    PRINT*, "No puedes ir en esa direccion"
+end if
+
+case ("sur")
+    print*
+    If (nom1(current_pos)=="2") then
+        print*, irsu
+        current_pos=sal1(current_pos)
+    else if (nom2(current_pos)=="2") then
+            print*, irsu
+            SELECT case (sal2(current_pos))
+            case ("1")
+            current_pos=1
+            case ("2")
+            current_pos=2
+            case ("3")
+            current_pos=3
+            case ("4")
+            current_pos=4
+            case ("5")
+            current_pos=5
+        end select
+    else if (nom3(current_pos)=="2") then
+        print*, irsu
+        SELECT case (sal3(current_pos))
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+        case ("6")
+        current_pos=6
+        end select
+    else 
+        PRINT*, "No puedes ir en esa direccion"
+    end if
+
+case ("este")
+    print*
+    If (nom1(current_pos)=="3") then
+        print*, ires
+        current_pos=sal1(current_pos)
+    else if (nom2(current_pos)=="3") then
+            print*, ires
+            SELECT case (sal2(current_pos))
+            case ("1")
+            current_pos=1
+            case ("2")
+            current_pos=2
+            case ("3")
+            current_pos=3
+            case ("4")
+            current_pos=4
+            case ("5")
+            current_pos=5
+        end select
+    else if (nom3(current_pos)=="3") then
+        print*, ires
+        SELECT case (sal3(current_pos))
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+        case ("6")
+        current_pos=6
+        end select
+    else 
+        PRINT*, "No puedes ir en esa direccion"
+    end if
+
+case ("oeste")
+    print*
+    If (nom1(current_pos)=="4") then
+        print*, iroe
+        current_pos=sal1(current_pos)
+    else if (nom2(current_pos)=="4") then
+            print*, iroe
+            SELECT case (sal2(current_pos))
+            case ("1")
+            current_pos=1
+            case ("2")
+            current_pos=2
+            case ("3")
+            current_pos=3
+            case ("4")
+            current_pos=4
+            case ("5")
+            current_pos=5
+        end select
+    else if (nom3(current_pos)=="4") then
+        print*, iroe
+        SELECT case (sal3(current_pos))
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+        case ("6")
+        current_pos=6
+        end select
+    else 
+        PRINT*, "No puedes ir en esa direccion"
+    end if
+
+case ("arriba")
+    print*
+    If (nom1(current_pos)=="5") then
+        print*, irar
+        current_pos=sal1(current_pos)
+    else if (nom2(current_pos)=="5") then
+            print*, irar
+            SELECT case (sal2(current_pos))
+            case ("1")
+            current_pos=1
+            case ("2")
+            current_pos=2
+            case ("3")
+            current_pos=3
+            case ("4")
+            current_pos=4
+            case ("5")
+            current_pos=5
+        end select
+    else if (nom3(current_pos)=="5") then
+        print*, irar
+        SELECT case (sal3(current_pos))
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+        case ("6")
+        current_pos=6
+        end select
+    else 
+        PRINT*, "No puedes ir en esa direccion"
+    end if
+
+case ("abajo")
+    print*
+    If (nom1(current_pos)=="6") then
+        print*, irab
+        current_pos=sal1(current_pos)
+    else if (nom2(current_pos)=="6") then
+            print*, irab
+            SELECT case (sal2(current_pos))
+            case ("1")
+            current_pos=1
+            case ("2")
+            current_pos=2
+            case ("3")
+            current_pos=3
+            case ("4")
+            current_pos=4
+            case ("5")
+            current_pos=5
+        end select
+    else if (nom3(current_pos)=="6") then
+        print*, irab
+        SELECT case (sal3(current_pos))
+        case ("2")
+        current_pos=2
+        case ("3")
+        current_pos=3
+        case ("4")
+        current_pos=4
+        case ("5")
+        current_pos=5
+        case ("6")
+        current_pos=6
+        end select
+    else 
+        PRINT*, "No puedes ir en esa direccion"
+    end if
+
+case ("salir")
+    if (nom3(current_pos)=="s") then
+        print*
+        print*, "Ganaste, saliste del laberito"
+        print*, "Software desarrollado en FORTRAN por Lucas M. B. Borches"
+        print*, "Ingrese cualquier tecla para salir"
+        read*, dummy
+        exit
+    else
+        print*
+        print*, "La salida no se encuentra en esta habitacion"
+
+    end if
+
+case ("cerrar")
+    exit
+
+case ("mirar")
+    print*
+    print*, "Te encuentras en un laberinto subterraneo lleno de salas identicas"
+    print*
+
+    CASE DEFAULT
+        print*
+          PRINT *, "No entiendo ese comando"
+          print*, "Podes usar los comandos ARRIBA, ABAJO, NORTE, SUR, ESTE, OESTE, MIRAR, SALIR y CERRAR (en minusculas)"
+          print*
+end select
+
+
+end do
+
+
+
 
 contains
 
